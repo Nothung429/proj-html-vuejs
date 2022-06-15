899d97c35b4e1b610fd1398899645caa214dca57
Nothung429 <frank.gatto96.fg@gmail.com>
2022-06-15 12:12:56 +0200

    add

---
+++ src/components/sections/header/HeaderJumbo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="jumbo">
+        <div class="jumbo__hero">
+            <p>Development</p>
+            <h1>A Complete Developer Toolset</h1>
+            <p>Launch your campaign and benefit from our expertise on designing and managing conversion centered bootstrap4 html page.</p>
+            <button>Get started</button>
+            <button>Documentation</button>
+            <p>Current Version: v2.6.0</p>
+        </div>
+        <div class="jumbo__img">
+            <img src="../../../assets/img/about.png" alt="jumbo img">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HeaderJumbo",
+    }
+</script>
+
+<style lang="scss" scoped>
+    .jumbo {
+        display: flex;
+    }
+</style>
---
+++ src/components/sections/header/HeaderNavbar.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="navbar">
+        <div class="navbar__logo">
+            <img src="../../../assets/img/logo-dark.png" alt="header logo">
+        </div>
+        <div class="navbar__links">
+            <ul>
+                <li>home</li>
+                <li>home</li>
+                <li>home</li>
+                <li>home</li>
+            </ul>
+        </div>
+        <div class="navbar__buttons">
+            <button><i class="fa-solid fa-magnifying-glass"></i></button>
+            <button><i class="fa-brands fa-github"></i></button>
+            <button><i class="fa-brands fa-stack-overflow"></i></button>
+            <button><i class="fa-regular fa-user"></i></button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HeaderNavbar",
+    }
+</script>
+
+<style lang="scss" scoped>
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100px;
+        background-color: lightgreen;
+        &__logo {
+            max-width: 180px;
+        }
+        &__links {
+            ul {
+                display: flex;
+                li {
+                    padding: 0 15px;
+                }
+            }
+        }
+        &__buttons {
+            button {
+                font-size: 36px;
+            }
+        }
+    }
+</style>
