9c961fa31d55db46c0426c46f49ec3cb7a9db4c3
Nothung429 <frank.gatto96.fg@gmail.com>
2022-06-15 11:43:43 +0200

    init vue cli

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+
+<template>
+    <div id="app">
+        <div class="container">
+            <BaseHeader/>
+            <BaseMain/>
+            <BaseFooter/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import BaseHeader from "./components/blocks/BaseHeader.vue"
+    import BaseMain from "./components/blocks/BaseMain.vue"
+    import BaseFooter from "./components/blocks/BaseFooter.vue"
+        
+    export default {
+        name: "App",
+        components: {
+            BaseHeader,
+            BaseMain,
+            BaseFooter,
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import "assets/style/general.scss";
+</style>
