598e027eac6a42c95857225a9748a374c3e10205
Nothung429 <frank.gatto96.fg@gmail.com>
2022-06-17 16:00:40 +0200

    add style and content

--- src/components/sections/main/MainNews.vue
+++ src/components/sections/main/MainNews.vue
@@ -1,9 +1,9 @@
 <template>
-    <section class="my-5">
-        <h2>Want to be among the first to find out?</h2>
-        <p>Start working with <span class="logo-name">Landrick</span> that can provide everything you need to generate awereness, drive traffic, connect</p>
+    <section class="news my-5">
+        <h2 class="pt-3">Want to be among the first to find out?</h2>
+        <p class="pb-3">Start working with <span class="logo-name">Landrick</span> that can provide everything you need to generate awereness, drive traffic, connect</p>
         <input type="text" placeholder="Email Address">
-        <button></button>
+        <button class="start">Get Started</button>
     </section>
 </template>
 
@@ -14,18 +14,25 @@
 </script>
 
 <style lang="scss" scoped>
-    section {
+    .news {
+        position: relative;
         text-align: center;
         input {
             width: 50%;
             border-radius: 30px;
             border: 1px solid #000;
-            padding: 10px 20px;
+            padding: 10px 20px;            
         }
-        button {
-            margin-left: -60px;
-            height: 30px;
-            width: 50px;
-        }
+        .start {
+            position: absolute;
+            top: 124px;
+            right: 337px;
+            border-radius: 30px;
+            padding: 5px 10px;
+            box-shadow: 3px 3px 3px #8492a6;
+            background-color: #2f55d4;
+            color: #f8f9fc;
+            border: none;
+        }        
     }
 </style>
